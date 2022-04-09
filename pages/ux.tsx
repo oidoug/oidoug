c938d7d35143705786825a42a54c0adfc0494614
Douglas Schmidt <douglaslondrina@gmail.com>
2022-04-09 11:49:24 -0300

    Updating post#1 copy

--- pages/ux.tsx
+++ pages/ux.tsx
@@ -15,74 +15,90 @@ const Home: NextPage = () => {
         From the end of 2010 up to around 2013, I studied Interaction Design in
         a trendy new design school called Faber-Ludens Institute, now
         unfortunately extinct. Their students, or Fabers, were thinkers and
-        tinkerers coming from Design or IT bachelors looking for discovering new
-        frontiers in Human-Computer-Interactions. To us, students, Faber-Ludens
-        was more than just a grad school, it was almost a cult to people and
-        their lives, desires, and needs. We focused on understanding people
-        through anthropology and psychology lenses.
+        tinkerers coming from Design or IT bachelors looking for new frontiers
+        in Human-Computer-Interactions. To its students, Faber-Ludens was more
+        than just a graduation program, it was almost a philosophy, a cult to
+        people and their lives, needs, and desires. We focused on understanding
+        people through anthropology and psychology.
       </Text>
       <Spacer y={2} />
       <Text>
-        At that time, design to me was so simple and enjoyable. With my
-        colleagues, we had deep discussions about crazy ideas, we talked to
-        people, we prototyped crazy stuff and we validated that whatever we
-        produced, in a cycle of research, ideation, prototyping, and validation.
-        We were students, so we had freedom and little pressure to create, but
-        still, from time to time, some amazing projects surfaced there. I think
-        that the laid-back environment that favored open-minded thinking had the
-        perfect ingredients for great innovation. Faber-Ludens Institute was way
-        ahead of its time, so it had a hard time fitting in more traditional
-        colleges programs.
+        At that time, design to me was so enjoyable. With my colleagues, we had
+        deep discussions about crazy new ideas; we talked to people, prototyped
+        crazy new stuff, and validated whatever we produced, in a cycle of
+        research, ideation, prototyping, and validation.
       </Text>
       <Spacer y={2} />
       <Text>
-        Moving forward, I had the chance to work with new products and teams as
-        a User Experience Designer, Interaction Design&apos;s better-known
-        cousin.
+        We were students, so we had freedom and little pressure to create viable
+        products, but still, from time to time, some amazing projects surfaced
+        there. In retrospect, it&apos;s easy to see that the laid-back
+        environment that favored open-minded thinking and discussions had the
+        perfect ingredients for innovation. Faber-Ludens Institute was way ahead
+        of its time, so it had a hard time fitting into more traditional college
+        programs.
+      </Text>
+      <Spacer y={2} />
+      <Text>
+        Moving forward, in my professional life, along with working as a Mobile
+        Developer, I worked on new products as a User Experience Designer,
+        Interaction Design&apos;s better-known cousin, and as a founder,
+        conciliating creative ideas with what would generate value for both
+        users and company.
       </Text>
       <Spacer y={2} />
       <Text>
         UX as a field has grown a lot and has spawned whole new divisions in big
-        companies. And that is really great, it shows how much designing from
-        the user&apos;s needs perspective has a positive impact on companies,
+        companies. And that is great, it shows how much designing from the
+        user&apos;s needs perspective has a positive impact on companies,
         services, and products.
       </Text>
       <Spacer y={2} />
       <Text>
-        But that popularity has to lead us to a new problem. Designing UX
+        But UX&apos;s popularity led to new problems. Designing UX
         professionally had become bureaucratic and boring. There are so many new
-        processes and deliverables for the team to take note of and produce,
-        that the basic Interaction Design ideals are now forgotten.
+        processes and deliverables for the team to keep track of and produce,
+        that the basic Interaction Design values are now forgotten. Designers
+        need constant reminders to not forget to talk to users, and
+        designer-centered design is abundant.
+      </Text>
+      <Spacer y={2} />
+      <Text>
+        I too fell into this trap many many times. With so many new tools,
+        processes, and deliverables we want to push out, it&apos;s easy to get
+        immersed in our assumptions.
       </Text>
       <Spacer y={2} />
       <Text>
         So that&apos;s why, in my interpretation, some designers started doing a
-        leaner version of the known UX processes. The concept of Lean UX
-        isn&apos;t new and I first read about it in this post from Jeff Gothelf
-        from 2011, and it probably was the biggest influence on Jake
-        Knapp&apos;s Design Sprint methodology, which I&apos;m also a big fan.
+        leaner version of UX processes. The concept of Lean UX isn&apos;t new, I
+        first read about it some time ago in this post from Jeff Gothelf from
+        2011! And it probably had some influence on Jake Knapp&apos;s Design
+        Sprint methodology, of which I&apos;m also fond.
       </Text>
       <Spacer y={2} />
       <Text>
-        Regardless of the method or how we call it, we need to recover that
-        sense of understanding people&apos;s desires and needs and create proper
-        innovative environments for new interaction solutions to appear, that
-        are again user-centered.
+        Regardless of the method or what we call it, we want to recover that
+        sense of understanding of people&apos;s needs and desires before
+        anything else, so new interactions and more creativity can emerge.
       </Text>
       <Spacer y={2} />
       <Text>
-        In practice, what I as a designer do is focus on what brings the most
-        value the faster, without prejudice on the method or without letting the
-        process get between the user and the end result. That may mean sometimes
-        to only use paper or to avoid doing wireframes or even creating personas
-        that will end up limiting the scope of our product. Instead, we go out,
-        observe, take notes, and quickly prototype.
+        In practice, what I&apos;m doing as a designer, is focusing on what
+        leads to the most valuable ideas, without prejudice on the method or
+        without letting the process get between the user and the result. We want
+        to quickly iterate, so we have more chances of talking to people and
+        getting their feedback. That may mean sometimes we only use paper to
+        prototype, or we avoid doing wireframes, diagrams, or personas, which
+        will inevitably get outdated, in favor of spending more time out,
+        observing, taking notes, to then prototype and validate.
       </Text>
       <Spacer y={2} />
       <Text>
-        There&apos;s actually no secret sauce. Successful founders have been
-        doing that empirically since always. We designers complicate stuff for
-        several and no reason at all.
+        There&apos;s no secret sauce, it&apos;s just a matter of caring about
+        people and not your portfolio. Successful founders have been doing that
+        empirically for a long time. We, designers and developers, complicate
+        stuff for several and no reason at all.
       </Text>
       <Spacer y={2} />
       <Text>Douglas A. Schmidt</Text>
